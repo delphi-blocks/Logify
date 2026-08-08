f3f63bbb54488e741a1361f842f4f787d844837c
paolo-rossi <paolo@paolorossi.net>
2026-08-08 11:13:33 +0200

    Compile (and operate) the Debug adapter on Linux

--- Source/Logify.Adapter.Debug.pas
+++ Source/Logify.Adapter.Debug.pas
@@ -43,16 +43,41 @@ type
 implementation
 
 uses
-  Winapi.Windows;
+  {$IFDEF MSWINDOWS}Winapi.Windows{$ENDIF}
+  {$IFDEF POSIX}Posix.Unistd{$ENDIF};
+
+/// <summary>
+///   Writes one line to the platform debug channel.
+/// </summary>
+procedure WriteDebugLine(const AMessage: string);
+{$IFDEF POSIX}
+var
+  LBytes: TBytes;
+{$ENDIF}
+begin
+  {$IFDEF MSWINDOWS}
+  // The Windows debug channel is line oriented: one call is one line
+  OutputDebugString(PChar(AMessage));
+  {$ENDIF}
+
+  {$IFDEF POSIX}
+  // There is no OutputDebugString here. stderr is the conventional out of
+  // band diagnostic channel: unbuffered, separate from whatever the program
+  // writes to stdout, and captured by the IDE when debugging a Linux target.
+  // A single write() call per message keeps concurrent lines from interleaving.
+  LBytes := TEncoding.UTF8.GetBytes(AMessage + sLineBreak);
+  __write(STDERR_FILENO, Pointer(LBytes), Length(LBytes));
+  {$ENDIF}
+end;
 
 procedure TLogifyAdapterDebug.InternalLog(const AMessage, AClassName: string; AException: Exception; ALevel: TLogLevel);
 begin
-  OutputDebugString(PChar(FormatMsg(AMessage, AClassName, AException, ALevel)));
+  WriteDebugLine(FormatMsg(AMessage, AClassName, AException, ALevel));
 end;
 
 procedure TLogifyAdapterDebug.InternalRaw(const AMessage: string; ALevel: TLogLevel);
 begin
-  OutputDebugString(PChar(AMessage));
+  WriteDebugLine(AMessage);
 end;
 
 { TLogifyAdapterDebugFactory }
